9f46824f12072c916a3cf623e39cbe7a3006236b
kindlin2012 <13988390600@139.com>
2022-01-17 00:52:31 +0000

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -42,10 +42,29 @@ class UsersController extends Controller
         //等价于redirect()->route('users.show', [$user->id]);
     }
 
-    public function destroy()
+    public function edit(User $user)
     {
-        Auth::logout();
-        session()->flash('success', '您已成功退出！');
-        return redirect('login');
+        return view('users.edit', compact('user'));
     }
+
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show', $user);
+    }
+
+
 }
